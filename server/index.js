32fe35ca65f143317654d5b8114662b5f8dd4a59
caarlb <s.caarlb@gmail.com>
2022-02-20 14:38:18 -0600

    set up connection with atlas mongoDB

--- server/index.js
+++ server/index.js
@@ -8,3 +8,14 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://testing:testing123@cluster0.n6gdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error));

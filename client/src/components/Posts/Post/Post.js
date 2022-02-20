ef094887292fcdfb5d414292c2b94be2744ba541
caarlb <s.caarlb@gmail.com>
2022-02-20 17:23:46 -0600

    adding design for tasks and buttons for edit and delete

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,13 +1,56 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
-const Post = () => {
-    const classes = useStyles();
+const Post = ({ post }) => {
+  const classes = useStyles();
 
-    return (
-        <h1>This is a task</h1>
-    )
-}
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.tittle}</Typography>
+      </div>
+      <CardActions className={classes.cardActions}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {}}
+        >
+          <MoreHorizIcon fontSize="small" />Edit {post.likeCount}{" "}
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {}}
+        >
+          <DeleteIcon fontSize="small" /> Delete
+        </Button>
+      </CardActions>
+
+      {/* 
+      <div className={classes.overlay2}>
+        <Button style={{color:'white'}} size="small" onClick={() => {}}>
+            <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div> */}
+    </Card>
+  );
+};
 
 export default Post;

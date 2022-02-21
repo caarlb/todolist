b6fdbcfe4f69f2050dc2f61907c76e744d018e89
caarlb <s.caarlb@gmail.com>
2022-02-20 19:58:21 -0600

    delete option added and working

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -14,10 +14,14 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
+import { useDispatch } from 'react-redux';
 
 import useStyles from "./styles";
 
+import { deletePost } from '../../../actions/posts';
+
 const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
 
   return (
@@ -31,7 +35,7 @@ const Post = ({ post, setCurrentId }) => {
           <Button size="small" color="primary" onClick={() => setCurrentId(post._id)}>
             <MoreHorizIcon fontSize="small" /> Edit
           </Button>
-          <Button size="small" color="primary" onClick={() => {}}>
+          <Button size="small" color="primary" onClick={() =>  dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize="small" /> Delete
           </Button>
         </CardActions>
